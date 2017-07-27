b9e270467748f5179c22f05ebb34e6881326de66
vivek <rajvivek.v@gmail.com>
2017-07-27 22:01:01 +0530

    change the following word Crowdbotics

--- rails4samples/customers_controller.rb
+++ rails4samples/customers_controller.rb
@@ -63,7 +63,7 @@ class CustomersController < ApplicationController
   def destroy
     @customer.destroy
     respond_to do |format|
-      format.html { redirect_to customers_url, notice: "Customer was successfully destroyed. "}
+      format.html { redirect_to customers_url, notice: "Crowdbotics"}
       format.json { head :no_content }
     end
   end
